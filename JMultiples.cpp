15ee05316629bce62082dcb9bb1b48cddbfd1d7c
abdallhmostafa <abdallhelrabiey255@gmail.com>
2023-10-06 15:31:15 +0300

    first commit

---
+++ JMultiples.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	// ICPC Assiut University Community //
+// --------------------	J. Multiples -------------------- //
+
+	long double x, y;
+
+
+	cin >> x >> y;
+
+
+	;
+	if ((x / y) == (int)(x / y) || (y / x) == (int)(y / x)) {
+		cout << "Multiples" << "\n";
+	}
+	else
+	{
+		cout << "No Multiples" << "\n";
+
+	}
+
+}
